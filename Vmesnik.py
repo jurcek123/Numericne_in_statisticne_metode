6d62bd3d7dcf244196301bf254620cbfe007cf37
bubblebeam11 <kristijansaver@gmail.com>
2018-05-14 16:33:20 +0200

    Add files via upload

--- Vmesnik.py
+++ Vmesnik.py
@@ -5,23 +5,24 @@ from math import *
 
 class Metode:
     def __init__(self, master):
-        
+
         self.root = master
         self.okno = None
-        menu = Menu(master)    
-        master.config(menu = menu)
+        menu = Menu(master)
+        master.config(menu=menu)
         menuMetode = Menu(menu)
         menuMetode = Menu(menu, tearoff=0)
         menu.add_cascade(label="Metode", menu=menuMetode)
-        menuMetode.add_command(label="Bisekcija",command = self.PodatkiBisekcija)
-        menuMetode.add_command(label="LU razcep DP",command= self.VelikostMatrikeBP)
-        menuMetode.add_command(label="LU razcep BP",command = self.VelikostMatrikeDP)
+        menuMetode.add_command(label="Bisekcija", command=self.PodatkiBisekcija)
+        menuMetode.add_command(label="LU razcep DP", command=self.VelikostMatrikeDP)
+        menuMetode.add_command(label="LU razcep BP", command=self.VelikostMatrikeBP)
         menuMetode.add_command(label="QR razcep", command=self.VelikostMatrikeQR)
         menuMetode.add_command(label="Cholesky", command=self.VelikostMatrikeCh)
 
         menuKoncaj = Menu(menu, tearoff=0)
         menu.add_cascade(label="Končaj", menu=menuKoncaj)
         menuKoncaj.add_command(label="Quit", command=master.destroy)
+
     def PodatkiBisekcija(self):
         okno = Frame(root)
         okno.pack()
@@ -59,24 +60,29 @@ class Metode:
         self.vrstice.grid(row=1, column=2)
         self.stolpci = Entry(self.okno)
         self.stolpci.grid(row=2, column=2)
-        self.PrviGumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaBP).grid(row=3, column=2)
-
+        self.gumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaBP).grid(row=3, column=2)
 
     def PodatkiMatrikaBP(self):
         '''ustvari okno nxn, kamor vnesemo podatke'''
+        if self.okno != None:
+            self.okno.pack_forget()
+        self.okno = Frame(self.root)
+        self.okno.pack()
+        if self.gumb!=None:
+            self.gumb.destroy()
         vrstice = int(self.vrstice.get())
         stolpci = int(self.stolpci.get())
         self.A = [[] for x in range(vrstice)]
         for i in range(vrstice):
             for j in range(stolpci):
                 self.A[i].append(Entry(self.okno))
-                self.A[i][j].grid(row = i, column = j)
+                self.A[i][j].grid(row=i, column=j)
         print('matrika A: ' + str(self.A))
         Button(self.okno, text='Potrdi', command=self.LU_razcep_bp).grid(row=2, column=2)
 
     def LU_razcep_bp(self):
         '''vzame podatke iz PodatkiMatrika in izvede LUbp razcep, ki ga nato izpiše'''
-        if self.okno!=None:
+        if self.okno != None:
             self.okno.pack_forget()
         self.okno = Frame(self.root)
         self.okno.pack()
@@ -92,7 +98,8 @@ class Metode:
         det = np.linalg.det(self.A)
         # za LU razcep potrebujemo nesingularno matriko
         if det == 0:
-            Label(self.okno,text = 'matrika je singularna, LU razcep ni izvedljiv.', font=('Helvetica', 16)).grid(row=2, column=1)
+            Label(self.okno, text='matrika je singularna, LU razcep ni izvedljiv.', font=('Helvetica', 16)).grid(row=2,
+                                                                                                                 column=1)
             return 'matrika je singularna, LU razcep ni izvedljiv.'
         else:
             n = self.A.shape[1]
@@ -102,23 +109,21 @@ class Metode:
                     L[i, j] = self.A[i, j] / self.A[j, j]
                     for k in range(j + 1, n):
                         self.A[i, k] = self.A[i, k] - L[i, j] * self.A[j, k]
-            print(L,np.triu(self.A))
+            print(L, np.triu(self.A))
 
             # izpišemo razcepljeno matriko
-            Label(self.okno, text='spodnje trikotna matrika L:',font=("Helvetica", 16)).grid(row=1, column=1)
-            Label(self.okno, text=str(L),font=("Helvetica", 16)).grid(row=2, column=1)
-            Label(self.okno, text='zgornje trikotna matrika U:',font=("Helvetica", 16)).grid(row=n+1, column=1)
-            Label(self.okno, text=str(np.triu(self.A)),font=("Helvetica", 16)).grid(row=n+2, column=1)
-
-
+            Label(self.okno, text='spodnje trikotna matrika L:', font=("Helvetica", 16)).grid(row=1, column=1)
+            Label(self.okno, text=str(L), font=("Helvetica", 16)).grid(row=2, column=1)
+            Label(self.okno, text='zgornje trikotna matrika U:', font=("Helvetica", 16)).grid(row=n + 1, column=1)
+            Label(self.okno, text=str(np.triu(self.A)), font=("Helvetica", 16)).grid(row=n + 2, column=1)
 
             return L, np.triu(self.A)
 
 
-#---------------------------------------------------------------------------------------------------------------
+            # ---------------------------------------------------------------------------------------------------------------
 
-#*********LU razcep z delnim pivotiranjem**********
-#---------------------------------------------------------------------------------------------------------------
+            # *********LU razcep z delnim pivotiranjem**********
+            # ---------------------------------------------------------------------------------------------------------------
 
     def VelikostMatrikeDP(self):
         '''najprej nas zanima velikost matrike'''
@@ -136,10 +141,16 @@ class Metode:
         self.vrstice.grid(row=1, column=2)
         self.stolpci = Entry(self.okno)
         self.stolpci.grid(row=2, column=2)
-        self.PrviGumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaBP).grid(row=3, column=2)
+        self.gumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaBP).grid(row=3, column=2)
 
     def PodatkiMatrikaDP(self):
         '''ustvari okno nxn, kamor vnesemo podatke'''
+        if self.okno != None:
+            self.okno.pack_forget()
+        self.okno = Frame(self.root)
+        self.okno.pack()
+        if self.gumb!=None:
+            self.gumb.destroy()
         vrstice = int(self.vrstice.get())
         stolpci = int(self.stolpci.get())
         self.A = [[] for x in range(vrstice)]
@@ -150,21 +161,24 @@ class Metode:
         print('matrika A: ' + str(self.A))
         Button(self.okno, text='Potrdi', command=self.LU_razcep_dp).grid(row=2, column=2)
         return None
+
     def LU_razcep_dp(self):
         '''vzame podatke iz PodatkiMatrika in izvede LUbp razcep, ki ga nato izpiše'''
-        if self.okno!=None:
+        if self.okno != None:
             self.okno.pack_forget()
         self.okno = Frame(self.root)
         self.okno.pack()
+        if self.gumb!=None:
+            self.gumb.destroy()
         vrstice = int(self.vrstice.get())
         stolpci = int(self.stolpci.get())
         self.B = [[] for x in range(vrstice)]
         for i in range(vrstice):
-            for i in range(j+1,n):
-                if abs(A[p,j])<abs(A[i,j]):
+            for i in range(j + 1, n):
+                if abs(A[p, j]) < abs(A[i, j]):
                     p = i
                 A[[j, p]] = A[[p, j]]
-                P[[j, p]] = P[[p,j]]
+                P[[j, p]] = P[[p, j]]
             for j in range(stolpci):
                 self.B[i].append(float(self.A[i][j].get()))
 
@@ -173,7 +187,8 @@ class Metode:
         det = np.linalg.det(self.A)
         # za LU razcep potrebujemo nesingularno matriko
         if det == 0:
-            Label(self.okno,text = 'matrika je singularna, LU razcep ni izvedljiv.', font=('Helvetica', 16)).grid(row=2, column=1)
+            Label(self.okno, text='matrika je singularna, LU razcep ni izvedljiv.', font=('Helvetica', 16)).grid(row=2,
+                                                                                                                 column=1)
             return 'matrika je singularna, LU razcep ni izvedljiv.'
         else:
             n = self.A.shape[1]
@@ -183,20 +198,18 @@ class Metode:
                     L[i, j] = self.A[i, j] / self.A[j, j]
                     for k in range(j + 1, n):
                         self.A[i, k] = self.A[i, k] - L[i, j] * self.A[j, k]
-            print(L,np.triu(self.A))
+            print(L, np.triu(self.A))
 
             # izpišemo razcepljeno matriko
-            Label(self.okno, text='spodnje trikotna matrika L:',font=("Helvetica", 16)).grid(row=1, column=1)
-            Label(self.okno, text=str(L),font=("Helvetica", 16)).grid(row=2, column=1)
-            Label(self.okno, text='zgornje trikotna matrika U:',font=("Helvetica", 16)).grid(row=n+1, column=1)
-            Label(self.okno, text=str(np.triu(self.A)),font=("Helvetica", 16)).grid(row=n+2, column=1)
-
-
+            Label(self.okno, text='spodnje trikotna matrika L:', font=("Helvetica", 16)).grid(row=1, column=1)
+            Label(self.okno, text=str(L), font=("Helvetica", 16)).grid(row=2, column=1)
+            Label(self.okno, text='zgornje trikotna matrika U:', font=("Helvetica", 16)).grid(row=n + 1, column=1)
+            Label(self.okno, text=str(np.triu(self.A)), font=("Helvetica", 16)).grid(row=n + 2, column=1)
 
             return L, np.triu(self.A)
 
-#*********QR razcep**********
-#---------------------------------------------------------------------------------------------------------------
+            # *********QR razcep**********
+            # ---------------------------------------------------------------------------------------------------------------
 
     def VelikostMatrikeQR(self):
         '''najprej nas zanima velikost matrike'''
@@ -214,10 +227,16 @@ class Metode:
         self.vrstice.grid(row=1, column=2)
         self.stolpci = Entry(self.okno)
         self.stolpci.grid(row=2, column=2)
-        self.PrviGumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaQR).grid(row=3, column=2)
+        self.gumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaQR).grid(row=3, column=2)
 
     def PodatkiMatrikaQR(self):
         '''ustvari okno nxn, kamor vnesemo podatke'''
+        if self.okno != None:
+            self.okno.pack_forget()
+        self.okno = Frame(self.root)
+        self.okno.pack()
+        if self.gumb!=None:
+            self.gumb.destroy()
         vrstice = int(self.vrstice.get())
         stolpci = int(self.stolpci.get())
         self.A = [[] for x in range(vrstice)]
@@ -228,15 +247,18 @@ class Metode:
         print('matrika A: ' + str(self.A))
         Button(self.okno, text='Potrdi', command=self.QR_razcep).grid(row=2, column=2)
         return None
+
     def QR_razcep(self):
         '''vzame podatke iz PodatkiMatrika in izvede QR razcep, ki ga nato izpiše'''
+
         def Householder(a):
             v = a / (a[0] + np.copysign(np.linalg.norm(a), a[0]))
             v[0] = 1
             H = np.eye(a.shape[0])
             H -= (2 / np.dot(v, v)) * np.dot(v[:, None], v[None, :])
             return H
-        if self.okno!=None:
+
+        if self.okno != None:
             self.okno.pack_forget()
         self.okno = Frame(self.root)
         self.okno.pack()
@@ -263,12 +285,13 @@ class Metode:
         Label(self.okno, text=str(Q.round(4))[1:-1], font=("Helvetica", 16)).grid(row=2, column=1)
         Label(self.okno, text='Matrika R:', font=("Helvetica", 16)).grid(row=n + 1, column=1)
         Label(self.okno, text=str(R.round(4))[1:-1], font=("Helvetica", 16)).grid(row=n + 2, column=1)
-        return Q, A
+        return Q, R
 
-# ---------------------------------------------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------------------------------------------
 
-# *********razcep choleskega**********
-# ---------------------------------------------------------------------------------------------------------------
+    # *********razcep choleskega**********
+
+    # ---------------------------------------------------------------------------------------------------------------
 
     def VelikostMatrikeCh(self):
         '''najprej nas zanima velikost matrike'''
@@ -286,22 +309,33 @@ class Metode:
         self.vrstice.grid(row=1, column=2)
         self.stolpci = Entry(self.okno)
         self.stolpci.grid(row=2, column=2)
-        self.PrviGumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaCh).grid(row=3, column=2)
+        self.gumb = Button(self.okno, text='Potrdi', command=self.PodatkiMatrikaCh).grid(row=3, column=2)
 
     def PodatkiMatrikaCh(self):
         '''ustvari okno nxn, kamor vnesemo podatke'''
+        if self.okno != None:
+            self.okno.pack_forget()
+        self.okno = Frame(self.root)
+        self.okno.pack()
+        if self.gumb!=None:
+            self.gumb.destroy()
         vrstice = int(self.vrstice.get())
         stolpci = int(self.stolpci.get())
-        self.A = [[] for x in range(vrstice)]
-        for i in range(vrstice):
-            for j in range(stolpci):
-                self.A[i].append(Entry(self.okno))
-                self.A[i][j].grid(row=i, column=j)
-        Button(self.okno, text='Potrdi', command=self.Cholesky).grid(row=2, column=2)
-        return None
+        if vrstice == stolpci:
+            self.A = [[] for x in range(vrstice)]
+            for i in range(vrstice):
+                for j in range(stolpci):
+                    self.A[i].append(Entry(self.okno))
+                    self.A[i][j].grid(row=i, column=j)
+            Button(self.okno, text='Potrdi', command=self.Cholesky).grid(row=2, column=2)
+            return None
+        else:
+            Label(self.okno, text='Razcep Choleskega je mogoč le z nxn simetrično pozitivno definitno matriko',
+                  font=("Helvetica", 16)).grid(row=1, column=1)
+
 
     def Cholesky(self):
-        if self.okno!=None:
+        if self.okno != None:
             self.okno.pack_forget()
         self.okno = Frame(self.root)
         self.okno.pack()
@@ -315,6 +349,8 @@ class Metode:
             for j in range(stolpci):
                 self.B[i].append(float(self.A[i][j].get()))
         A = self.B
+        print(A, np.transpose(A))
+
         L = [[0.0] * len(A) for _ in range(len(A))]
         for i, (Ai, Li) in enumerate(zip(A, L)):
             for j, Lj in enumerate(L[:i + 1]):
@@ -336,3 +372,5 @@ root = Tk()
 app = Metode(root)
 
 root.mainloop()
+
+
